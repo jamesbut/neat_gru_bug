44b81f25b8c0999306f63f5f462c6e1a7ea95fb7
James Butterworth <j.butterworth2@liv.ac.uk>
2018-09-12 10:21:28 +0100

    Changed time for exp

--- scripts/score_analysis_training.py
+++ scripts/score_analysis_training.py
@@ -2,7 +2,7 @@ from numpy import genfromtxt
 import matplotlib.pyplot as plt
 
 #Read data
-#train_file_data = genfromtxt('../scores/training_scores/archive_scores/ap_17/ap_17_training_1.txt', delimiter=',')
+train_file_data = genfromtxt('../scores/training_scores/training_scores.txt', delimiter=',')
 #train_file_data = genfromtxt('../scores/training_scores/archive_scores/ap_6/ap_6_training_6.txt', delimiter=',')
 #train_file_data = genfromtxt('../scores/training_scores/archive_scores/ap_6_f2/ap_6_f2_training_2.txt', delimiter=',')
 
